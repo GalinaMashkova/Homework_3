1186e5136d457c2be725db6d12af19a5380372a9
galina_mashkova <11966galina@gmail.com>
2023-01-06 20:50:52 +0300

    HW005

---
+++ HW_1/HW005/Program.cs
@@ -0,0 +1,21 @@
+﻿//Напишите программу, которая на вход
+//принимает число (N),
+// а на выходе показывает
+// все чётные числа от 1 до N.
+
+//5 -> 2, 4
+//8 -> 2, 4, 6, 8
+
+int [] array = {12, 31, 42, 54, 78};
+int N = array.Length;
+int index = 1;
+
+while (index <= N)
+{   
+    if (array[index] % 2 == 0) 
+   {
+    Console.WriteLine (array[index]);
+    }
+   
+    index = index + 1;
+}
