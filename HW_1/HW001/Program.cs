08a0cc1b716732c949f3f0512be494364aaf2ee5
galina_mashkova <11966galina@gmail.com>
2023-01-06 11:12:15 +0300

    HW001

--- HW_1/HW001/Program.cs
+++ HW_1/HW001/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+// a = 5; b = 7 -> max = 7
+// a = 2 b = 10 -> max = 10
+// a = -9 b = -3 -> max = -3
+
+int a = 31;
+int b = 5;
+int result = NullableComparer = a + b;
+Console.ErateLine(result);
