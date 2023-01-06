1b3e5bf04cbc611708ffc166c49f7d0e855d866d
galina_mashkova <11966galina@gmail.com>
2023-01-06 12:14:26 +0300

    задача на уроке 1

--- HW_1/HW001/Program.cs
+++ HW_1/HW001/Program.cs
@@ -1,10 +1,6 @@
-﻿// Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
-// a = 5; b = 7 -> max = 7
-// a = 2 b = 10 -> max = 10
-// a = -9 b = -3 -> max = -3
+﻿// Сложение чисел
 
-//int a = 31;
-//int b = 5;
-//int result = NullableComparer = a + b;
-//Console.WriteLine(result);
-Задача
+int a = 31;
+int b = 5;
+int result = a + b;
+Console.WriteLine(result);
