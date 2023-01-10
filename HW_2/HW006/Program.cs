2cbf71360b4df4ed3197cc97536a0cb8cca15b6b
galina_mashkova <11966galina@gmail.com>
2023-01-10 17:25:23 +0300

    HW006

--- HW_2/HW006/Program.cs
+++ HW_2/HW006/Program.cs
@@ -2,9 +2,25 @@
 // и на выходе показывает вторую цифру этого числа.
 
 Console.WriteLine ("Введите трёхзначное число");
-string str;
-int number;
-str = Console.ReadLine(); //считываем число, которое вводим в консоль в строку str.
-number = Convert.ToInt32(str); //конвертируем строку и результат помещаем в переменую number.
-Console.WriteLine(str[1]);
+int number = Convert.ToInt32(Console.ReadLine());
+{
+    Console.WriteLine(number / 10 % 10);
+}
+
+//int number = 456;
+//int a = number / 100; // выводит первое число
+//int b = number / 10 % 10; // выводит второе число
+//int c = number % 10; // выводит третье число
+//Console.WriteLine(a);
+//Console.WriteLine(b);
+//Console.WriteLine(c);
+
+
+//Со строками не подходит
+//Console.WriteLine ("Введите трёхзначное число");
+//string str;
+//int number;
+//str = Console.ReadLine(); //считываем число, которое вводим в консоль в строку str.
+//number = Convert.ToInt32(str); //конвертируем строку и результат помещаем в переменую number.
+//Console.WriteLine(str[1]);
 //Console.WriteLine(str.Substring(1,1)); //или так можно
