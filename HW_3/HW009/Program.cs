42da8dbd565ed2e45fb8d087ac1212b2ae5bc3d7
galina_mashkova <11966galina@gmail.com>
2023-01-13 22:39:27 +0300

    HW011

--- HW_3/HW009/Program.cs
+++ HW_3/HW009/Program.cs
@@ -7,3 +7,21 @@
 // 23432 -> да
 // НЕЛЬЗЯ ИСПОЛЬЗОВАТЬ СТРОКИ
 
+Console.WriteLine("Введите пятизначное число ");
+int number = Convert.ToInt32(Console.ReadLine());
+bool Palindrome(int a)
+{
+    int b = 0;
+    int interval = a;
+    while (a > 0)
+    {
+        int quantity = a % 10;
+        a = a / 10;
+        b = b * 10 + quantity;
+    }
+    return interval == b;
+if(Palindrome(number))
+    Console.WriteLine("Число не является палиндромом");
+else 
+    Console.WriteLine("Число не является палиндромом");
+}
---
+++ HW_3/HW011/Program.cs
@@ -0,0 +1,66 @@
+﻿//Задача 23/ Напишите программу, которая принимает на вход число (N)
+// и выдаёт таблицу кубов чисел от 1 до N.
+
+//3 -> 1, 8, 27
+//5 -> 1, 8, 27, 64, 125
+
+// Считати от пользователя N
+int GetNumberByUser(string text)
+{
+    Console.Write(text + ": ");
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+// Создание массива
+int[] CreateArray(int size) 
+{
+    return new int [size];
+}
+ 
+//Метод получения кубов чисел
+void GetCubes(int[] ColNumber)
+{
+    int count = ColNumber.Length; //получение длины массива
+
+    for (int i = 1; i < count; i++)
+    {
+        ColNumber[i] = i * i * i;
+    }
+}
+
+// Распечатать результат
+string Print(int[] numbers)
+  {
+    string output = String.Empty;
+    int size = numbers.Length;
+
+    for (int i = 1; i < size; i++)
+    {
+output = output + $"{i}^3 = {numbers[i]}\n";
+    }
+ 
+    return output;
+ }
+
+string Print2(int[] numbers)
+  {
+    string output = String.Empty;
+    int size = numbers.Length;
+
+    for (int i = 1; i < size; i++)
+    {
+output = output + $"- {i}^3 = **{numbers[i]}**\n";
+    }
+ 
+    return output;
+ }
+
+int countByUser = GetNumberByUser("Введите число N");
+int[] list  = CreateArray(countByUser); // задаем массив
+GetCubes(list);
+string result = Print(list);
+Console.WriteLine(result);
+
+Console.WriteLine(); // создание пустой строки
+
+File.WriteAllText("list.md", Print2(list));
