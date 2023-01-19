19b4661a54da3d16546a5894fda5d8a712ee3489
galina_mashkova <11966galina@gmail.com>
2023-01-19 10:36:09 +0300

    HW017

---
+++ HW_7/HW018/Program.cs
@@ -0,0 +1,58 @@
+﻿//Задача 50. Напишите программу, которая на вход принимает позиции элемента
+// в двумерном массиве, и возвращает значение этого элемента или же указание,
+// что такого элемента нет.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 17 -> такого числа в массиве нет
+
+// Cоздаем массивы случайным образом
+void FillArray(int[,] array)
+{
+    for (int m = 0; m < array.GetLength(0); m++)
+    {
+        for (int n = 0; n < array.GetLength(1); n++)
+        {
+            array[m, n] = new Random().Next(1, 20);
+        }
+    }
+}
+
+// Печатаем массивы
+void PrintArray(int[,] array)
+{
+    for (int m = 0; m < array.GetLength(0); m++)
+    {
+        for (int n = 0; n < array.GetLength(1); n++)
+        {
+            Console.Write($"{array[m, n]} ");
+        }
+        Console.WriteLine();
+    }
+}
+// Присвоение переменным m и n цифр (например по 5)
+int m = 5; 
+int n = 5;
+int[,] array = new int[m, n];
+// Введение переменных
+FillArray(array);
+PrintArray(array);
+
+//Ввод числа и поиск его в массиах.
+Console.WriteLine("Введите число ");
+int num = int.Parse(Console.ReadLine()); 
+string Position(int[,] array)
+{
+    string result = string.Empty;
+    for (int m = 0; m < array.GetLength(0); m++)
+    {
+        for (int n = 0; n < array.GetLength(1); n++)
+        {
+            if (num == array[m, n]) result += $"({m}, {n})";
+        }
+    }
+    if (result == string.Empty) result = "Такого элемента нет";
+    return result;
+}
+Console.WriteLine(Position(array));
---
+++ HW_7/HW019/Program.cs
@@ -0,0 +1,62 @@
+﻿// Задача 52. Задайте двумерный массив из целых чисел.
+//Найдите среднее арифметическое элементов в каждом столбце.
+
+//Например, задан массив:
+//1 4 7 2
+//5 9 2 3
+//8 4 2 4
+//Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+// Cоздаем массивы случайным образом
+void FillArray(int[,] array)
+{
+    for (int m = 0; m < array.GetLength(0); m++)
+    {
+        for (int n = 0; n < array.GetLength(1); n++)
+        {
+            array[m, n] = new Random().Next(1, 20);
+        }
+    }
+}
+
+// Печатаем массивы
+void PrintArray(int[,] array)
+{
+    for (int m = 0; m < array.GetLength(0); m++)
+    {
+        for (int n = 0; n < array.GetLength(1); n++)
+        {
+            Console.Write($"{array[m, n]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+// Находим среднее арифметическое значение элементов в столбце
+string ArithmeticMean(int[,] array)
+{
+    string result = string.Empty;
+    for (int n = 0; n < array.GetLength(1); n++)
+    {
+        int sum = 0;
+        int count = 0;
+        double mean = 0;
+        for (int m = 0; m < array.GetLength(0); m++)
+        {
+            sum += array[m, n];
+            count++;
+        }
+        mean = sum / count;
+        result += mean.ToString() + " ";
+    }
+    return result;
+}
+// Присвоение переменным m и n цифр (например по 5)
+int m = 5;
+int n = 5;
+int[,] array = new int[m, n];
+// Введение переменных
+FillArray(array);
+PrintArray(array);
+Console.WriteLine();
+Console.WriteLine(ArithmeticMean(array));
